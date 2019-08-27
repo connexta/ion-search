beb9127d546c7c845adc0a1c06f63c505d600847
Josh Hunziker <joshah54@gmail.com>
2019-08-27 14:51:53 -0700

    parameterize bad request tests

--- src/test/java/com/connexta/search/index/IndexTests.java
+++ src/test/java/com/connexta/search/index/IndexTests.java
@@ -18,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 import javax.inject.Inject;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -35,15 +36,19 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -62,12 +67,16 @@ public class IndexTests {
   @Test
   public void testContextLoads() {}
 
-  @Test
-  public void testMissingFile() throws Exception {
+  @ParameterizedTest(name = "{0} index request returns {2}")
+  @MethodSource("badRequests")
+  void testBadRequests(
+      final String requestDescription,
+      final MockHttpServletRequestBuilder requestBuilder,
+      final HttpStatus expectedStatus)
+      throws Exception {
     mockMvc
         .perform(
-            multipart("/mis/product/00067360b70e4acfab561fe593ad3f7a/cst")
-                .header("Accept-Version", "0.1.0-SNAPSHOT")
+            requestBuilder
                 .with(
                     request -> {
                       request.setMethod(HttpMethod.PUT.toString());
@@ -75,54 +84,7 @@ public class IndexTests {
                     })
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.MULTIPART_FORM_DATA))
-        .andExpect(status().isBadRequest());
-  }
-
-  @Test
-  public void testInvalidProductId() throws Exception {
-    mockMvc
-        .perform(
-            multipart("/mis/product/1234/cst")
-                .file(
-                    new MockMultipartFile(
-                        "file",
-                        "test_file_name.txt",
-                        "text/plain",
-                        IOUtils.toInputStream(
-                            "All the color had been leached from Winterfell until only grey and white remained",
-                            StandardCharsets.UTF_8)))
-                .header("Accept-Version", "0.1.0-SNAPSHOT")
-                .with(
-                    request -> {
-                      request.setMethod(HttpMethod.PUT.toString());
-                      return request;
-                    })
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.MULTIPART_FORM_DATA))
-        .andExpect(status().isBadRequest());
-  }
-
-  @Test
-  public void testMissingVersion() throws Exception {
-    mockMvc
-        .perform(
-            multipart("/mis/product/00067360b70e4acfab561fe593ad3f7a/cst")
-                .file(
-                    new MockMultipartFile(
-                        "file",
-                        "test_file_name.txt",
-                        "text/plain",
-                        IOUtils.toInputStream(
-                            "All the color had been leached from Winterfell until only grey and white remained",
-                            StandardCharsets.UTF_8)))
-                .with(
-                    request -> {
-                      request.setMethod(HttpMethod.PUT.toString());
-                      return request;
-                    })
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.MULTIPART_FORM_DATA))
-        .andExpect(status().isBadRequest());
+        .andExpect(status().is(expectedStatus.value()));
   }
 
   @Test
@@ -131,41 +93,17 @@ public class IndexTests {
     // TODO verify 400
   }
 
-  @Test
-  public void testNonCST() throws Exception {
-    mockMvc
-        .perform(
-            multipart("/mis/product/00067360b70e4acfab561fe593ad3f7a/anotherMetadataType")
-                .file(
-                    new MockMultipartFile(
-                        "file",
-                        "test_file_name.txt",
-                        "text/plain",
-                        IOUtils.toInputStream(
-                            "All the color had been leached from Winterfell until only grey and white remained",
-                            StandardCharsets.UTF_8)))
-                .header("Accept-Version", "0.1.0-SNAPSHOT")
-                .with(
-                    request -> {
-                      request.setMethod(HttpMethod.PUT.toString());
-                      return request;
-                    })
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.MULTIPART_FORM_DATA))
-        .andExpect(status().isNotFound());
-  }
-
   /** @see SolrClient#query(String, SolrParams, METHOD) */
   @ParameterizedTest
   @ValueSource(classes = {IOException.class, SolrServerException.class, RuntimeException.class})
-  public void testSolrClientErrorsWhenQuerying(Class<? extends Throwable> throwable)
+  public void testSolrClientErrorsWhenQuerying(Class<? extends Throwable> throwableType)
       throws Exception {
     final String id = "00067360b70e4acfab561fe593ad3f7a";
     when(mockSolrClient.query(
             eq("searchTerms"),
             argThat(solrQuery -> StringUtils.equals(solrQuery.get("q"), "id:" + id)),
             eq(METHOD.GET)))
-        .thenThrow(throwable);
+        .thenThrow(throwableType);
 
     mockMvc
         .perform(
@@ -192,7 +130,7 @@ public class IndexTests {
   /** @see SolrClient#add(String, SolrInputDocument, int) */
   @ParameterizedTest
   @ValueSource(classes = {IOException.class, SolrServerException.class, RuntimeException.class})
-  public void testSolrClientErrorsWhenSaving(final Class<? extends Throwable> throwable)
+  public void testSolrClientErrorsWhenSaving(final Class<? extends Throwable> throwableType)
       throws Exception {
     final String id = "00067360b70e4acfab561fe593ad3f7a";
 
@@ -209,7 +147,7 @@ public class IndexTests {
     final String contents =
         "All the color had been leached from Winterfell until only grey and white remained";
     when(mockSolrClient.add(eq("searchTerms"), hasIndexFieldValues(id, contents), anyInt()))
-        .thenThrow(throwable);
+        .thenThrow(throwableType);
 
     mockMvc
         .perform(
@@ -238,5 +176,53 @@ public class IndexTests {
             StringUtils.equals((String) solrInputDocument.getField("id").getValue(), id)
                 && StringUtils.equals(
                     (String) solrInputDocument.getField("contents").getValue(), contents));
+  }
+
+  private static Stream<Arguments> badRequests() throws IOException {
+    // TODO test can't read attachment
+    return Stream.of(
+        Arguments.of(
+            "missing file",
+            multipart("/mis/product/00067360b70e4acfab561fe593ad3f7a/cst")
+                .header("Accept-Version", "0.1.0-SNAPSHOT"),
+            HttpStatus.BAD_REQUEST),
+        Arguments.of(
+            "invalid productId",
+            multipart("/mis/product/1234/cst")
+                .file(
+                    new MockMultipartFile(
+                        "file",
+                        "test_file_name.txt",
+                        "text/plain",
+                        IOUtils.toInputStream(
+                            "All the color had been leached from Winterfell until only grey and white remained",
+                            StandardCharsets.UTF_8)))
+                .header("Accept-Version", "0.1.0-SNAPSHOT"),
+            HttpStatus.BAD_REQUEST),
+        Arguments.of(
+            "missing Accept-Version",
+            multipart("/mis/product/00067360b70e4acfab561fe593ad3f7a/cst")
+                .file(
+                    new MockMultipartFile(
+                        "file",
+                        "test_file_name.txt",
+                        "text/plain",
+                        IOUtils.toInputStream(
+                            "All the color had been leached from Winterfell until only grey and white remained",
+                            StandardCharsets.UTF_8))),
+            HttpStatus.BAD_REQUEST),
+        Arguments.of(
+            "not cst",
+            multipart("/mis/product/00067360b70e4acfab561fe593ad3f7a/anotherMetadataType")
+                .file(
+                    new MockMultipartFile(
+                        "file",
+                        "test_file_name.txt",
+                        "text/plain",
+                        IOUtils.toInputStream(
+                            "All the color had been leached from Winterfell until only grey and white remained",
+                            StandardCharsets.UTF_8)))
+                .header("Accept-Version", "0.1.0-SNAPSHOT"),
+            HttpStatus.NOT_FOUND));
   }
 }
--- src/test/java/com/connexta/search/query/QueryTests.java
+++ src/test/java/com/connexta/search/query/QueryTests.java
@@ -14,6 +14,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.stream.Stream;
 import javax.inject.Inject;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.solr.client.solrj.SolrClient;
@@ -26,12 +28,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @ExtendWith(SpringExtension.class)
@@ -51,27 +57,22 @@ public class QueryTests {
   @Test
   public void testContextLoads() {}
 
-  @Test
-  public void testBadRequest() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/search")).andExpect(status().isBadRequest());
-  }
-
-  @Test
-  public void testEmptyKeyword() throws Exception {
-    final URIBuilder queryUriBuilder = new URIBuilder();
-    queryUriBuilder.setPath("/search");
-    queryUriBuilder.setParameter("q", "");
-    mockMvc
-        .perform(MockMvcRequestBuilders.get(queryUriBuilder.build()))
-        .andExpect(status().isBadRequest());
+  @ParameterizedTest(name = "{0} request returns {2}")
+  @MethodSource("badRequests")
+  public void testBadRequests(
+      final String requestDescription,
+      MockHttpServletRequestBuilder requestBuilder,
+      HttpStatus expectedStatus)
+      throws Exception {
+    mockMvc.perform(requestBuilder).andExpect(status().is(expectedStatus.value()));
   }
 
   /** @see SolrClient#query(SolrParams, METHOD) */
   @ParameterizedTest
   @ValueSource(classes = {IOException.class, SolrServerException.class, RuntimeException.class})
-  public void testSolrClientErrors(Class<? extends Throwable> throwable) throws Exception {
+  public void testSolrClientErrors(Class<? extends Throwable> throwableType) throws Exception {
     when(mockSolrClient.query(eq("searchTerms"), any(SolrQuery.class), eq(SolrRequest.METHOD.GET)))
-        .thenThrow(throwable);
+        .thenThrow(throwableType);
 
     final URIBuilder uriBuilder = new URIBuilder();
     uriBuilder.setPath("/search");
@@ -79,5 +80,18 @@ public class QueryTests {
     mockMvc
         .perform(MockMvcRequestBuilders.get(uriBuilder.build()))
         .andExpect(status().isInternalServerError());
+  }
+
+  private static Stream<Arguments> badRequests() throws URISyntaxException {
+    return Stream.of(
+        Arguments.of(
+            "missing search parameter",
+            MockMvcRequestBuilders.get("/search"),
+            HttpStatus.BAD_REQUEST),
+        Arguments.of(
+            "blank keyword",
+            MockMvcRequestBuilders.get(
+                new URIBuilder().setPath("/search").setParameter("q", "").build()),
+            HttpStatus.BAD_REQUEST));
   }
 }
