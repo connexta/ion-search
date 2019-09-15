c81bfdb56390e8431f10714d7744a7452cd6f8c4
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-09-14 19:34:51 -0700

    Copy down from master

--- src/test/java/com/connexta/search/index/IndexTests.java
+++ src/test/java/com/connexta/search/index/IndexTests.java
@@ -6,7 +6,7 @@
  */
 package com.connexta.search.index;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
@@ -63,6 +63,118 @@ public class IndexTests {
   @MockBean private SolrClient mockSolrClient;
 
   @Inject private MockMvc mockMvc;
+
+  @AfterEach
+  public void after() {
+    verifyNoMoreInteractions(ignoreStubs(mockSolrClient));
+  }
+
+  @Test
+  public void testContextLoads() {}
+
+  @ParameterizedTest(name = "{0} index request returns {2}")
+  @MethodSource("badRequests")
+  void testBadRequests(
+      final String requestDescription,
+      final MockHttpServletRequestBuilder requestBuilder,
+      final HttpStatus expectedStatus)
+      throws Exception {
+    mockMvc
+        .perform(
+            requestBuilder
+                .with(
+                    request -> {
+                      request.setMethod(HttpMethod.PUT.toString());
+                      return request;
+                    })
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.MULTIPART_FORM_DATA))
+        .andExpect(status().is(expectedStatus.value()));
+  }
+
+  @Test
+  @Disabled("TODO")
+  public void testCantReadAttachment() {
+    // TODO verify 400
+  }
+
+  /** @see SolrClient#query(String, SolrParams, METHOD) */
+  @ParameterizedTest
+  @ValueSource(classes = {IOException.class, SolrServerException.class, RuntimeException.class})
+  public void testSolrClientErrorsWhenQuerying(Class<? extends Throwable> throwableType)
+      throws Exception {
+    final String id = "00067360b70e4acfab561fe593ad3f7a";
+    when(mockSolrClient.query(
+            eq("searchTerms"),
+            argThat(solrQuery -> StringUtils.equals(solrQuery.get("q"), "id:" + id)),
+            eq(METHOD.GET)))
+        .thenThrow(throwableType);
+
+    mockMvc
+        .perform(
+            multipart("/mis/product/" + id + "/cst")
+                .file(
+                    new MockMultipartFile(
+                        "file",
+                        "test_file_name.json",
+                        "application/json",
+                        IOUtils.toInputStream(
+                            "{\"ext.extracted.text\" : \"All the color had been leached from Winterfell until only grey and white remained\"}",
+                            StandardCharsets.UTF_8)))
+                .header("Accept-Version", "0.1.0-SNAPSHOT")
+                .with(
+                    request -> {
+                      request.setMethod(HttpMethod.PUT.toString());
+                      return request;
+                    })
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.MULTIPART_FORM_DATA))
+        .andExpect(status().isInternalServerError());
+  }
+
+  // TODO fix this test. It fails for the wrong reason
+  /** @see SolrClient#add(String, SolrInputDocument, int) */
+  @Disabled
+  @ParameterizedTest
+  @ValueSource(classes = {IOException.class, SolrServerException.class, RuntimeException.class})
+  public void testSolrClientErrorsWhenSaving(final Class<? extends Throwable> throwableType)
+      throws Exception {
+    final String id = "00067360b70e4acfab561fe593ad3f7a";
+
+    final SolrDocumentList mockSolrDocumentList = mock(SolrDocumentList.class);
+    when(mockSolrDocumentList.size()).thenReturn(0);
+    final QueryResponse mockQueryResponse = mock(QueryResponse.class);
+    when(mockQueryResponse.getResults()).thenReturn(mockSolrDocumentList);
+    when(mockSolrClient.query(
+            eq("searchTerms"),
+            argThat(solrQuery -> StringUtils.equals(solrQuery.get("q"), "id:" + id)),
+            eq(METHOD.GET)))
+        .thenReturn(mockQueryResponse);
+
+    final String contents =
+        "{\"ext.extracted.text\" : \"All the color had been leached from Winterfell until only grey and white remained\"}";
+    when(mockSolrClient.add(eq("searchTerms"), hasIndexFieldValues(id, contents), anyInt()))
+        .thenThrow(throwableType);
+
+    mockMvc
+        .perform(
+            multipart("/mis/product/" + id + "/cst")
+                .file(
+                    new MockMultipartFile(
+                        "file",
+                        "test_file_name.json",
+                        "application/json",
+                        IOUtils.toInputStream(contents, StandardCharsets.UTF_8)))
+                .header("Accept-Version", "0.1.0-SNAPSHOT")
+                .with(
+                    request -> {
+                      request.setMethod(HttpMethod.PUT.toString());
+                      return request;
+                    })
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.MULTIPART_FORM_DATA))
+        .andExpect(status().isInternalServerError());
+  }
 
   private static SolrInputDocument hasIndexFieldValues(
       @NotEmpty final String id, @NotNull final String contents) {
@@ -121,123 +233,10 @@ public class IndexTests {
             HttpStatus.NOT_FOUND));
   }
 
-  @AfterEach
-  public void after() {
-    verifyNoMoreInteractions(ignoreStubs(mockSolrClient));
-  }
-
-  @Test
-  public void testContextLoads() {}
-
-  @ParameterizedTest(name = "{0} index request returns {2}")
-  @MethodSource("badRequests")
-  void testBadRequests(
-      final String requestDescription,
-      final MockHttpServletRequestBuilder requestBuilder,
-      final HttpStatus expectedStatus)
-      throws Exception {
-    mockMvc
-        .perform(
-            requestBuilder
-                .with(
-                    request -> {
-                      request.setMethod(HttpMethod.PUT.toString());
-                      return request;
-                    })
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.MULTIPART_FORM_DATA))
-        .andExpect(status().is(expectedStatus.value()));
-  }
-
-  // TODO fix this test. It fails for the wrong reason
-
-  @Test
-  @Disabled("TODO")
-  public void testCantReadAttachment() {
-    // TODO verify 400
-  }
-
-  /** @see SolrClient#query(String, SolrParams, METHOD) */
-  @ParameterizedTest
-  @ValueSource(classes = {IOException.class, SolrServerException.class, RuntimeException.class})
-  public void testSolrClientErrorsWhenQuerying(Class<? extends Throwable> throwableType)
-      throws Exception {
-    final String id = "00067360b70e4acfab561fe593ad3f7a";
-    when(mockSolrClient.query(
-            eq("searchTerms"),
-            argThat(solrQuery -> StringUtils.equals(solrQuery.get("q"), "id:" + id)),
-            eq(METHOD.GET)))
-        .thenThrow(throwableType);
-
-    mockMvc
-        .perform(
-            multipart("/mis/product/" + id + "/cst")
-                .file(
-                    new MockMultipartFile(
-                        "file",
-                        "test_file_name.json",
-                        "application/json",
-                        IOUtils.toInputStream(
-                            "{\"ext.extracted.text\" : \"All the color had been leached from Winterfell until only grey and white remained\"}",
-                            StandardCharsets.UTF_8)))
-                .header("Accept-Version", "0.1.0-SNAPSHOT")
-                .with(
-                    request -> {
-                      request.setMethod(HttpMethod.PUT.toString());
-                      return request;
-                    })
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.MULTIPART_FORM_DATA))
-        .andExpect(status().isInternalServerError());
-  }
-
-  /** @see SolrClient#add(String, SolrInputDocument, int) */
-  @Disabled
-  @ParameterizedTest
-  @ValueSource(classes = {IOException.class, SolrServerException.class, RuntimeException.class})
-  public void testSolrClientErrorsWhenSaving(final Class<? extends Throwable> throwableType)
-      throws Exception {
-    final String id = "00067360b70e4acfab561fe593ad3f7a";
-
-    final SolrDocumentList mockSolrDocumentList = mock(SolrDocumentList.class);
-    when(mockSolrDocumentList.size()).thenReturn(0);
-    final QueryResponse mockQueryResponse = mock(QueryResponse.class);
-    when(mockQueryResponse.getResults()).thenReturn(mockSolrDocumentList);
-    when(mockSolrClient.query(
-            eq("searchTerms"),
-            argThat(solrQuery -> StringUtils.equals(solrQuery.get("q"), "id:" + id)),
-            eq(METHOD.GET)))
-        .thenReturn(mockQueryResponse);
-
-    final String contents =
-        "{\"ext.extracted.text\" : \"All the color had been leached from Winterfell until only grey and white remained\"}";
-    when(mockSolrClient.add(eq("searchTerms"), hasIndexFieldValues(id, contents), anyInt()))
-        .thenThrow(throwableType);
-
-    mockMvc
-        .perform(
-            multipart("/mis/product/" + id + "/cst")
-                .file(
-                    new MockMultipartFile(
-                        "file",
-                        "test_file_name.json",
-                        "application/json",
-                        IOUtils.toInputStream(contents, StandardCharsets.UTF_8)))
-                .header("Accept-Version", "0.1.0-SNAPSHOT")
-                .with(
-                    request -> {
-                      request.setMethod(HttpMethod.PUT.toString());
-                      return request;
-                    })
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.MULTIPART_FORM_DATA))
-        .andExpect(status().isInternalServerError());
-  }
-
   @Test
   public void testExistingProduct() {
     CrudRepository crudRepository = mock(CrudRepository.class);
-    String productId = "123";
+    String productId = "00067360b70e4acfab561fe593afaded";
     doReturn(true).when(crudRepository).existsById(productId);
     IndexManagerImpl indexManager = new IndexManagerImpl(crudRepository);
     assertThrows(
